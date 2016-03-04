fa4f7055b9a70fe4ceff0b88b42f9ca1f66e7754
Richard Williams <richard.williams2@manchester.ac.uk>
2016-03-04 14:45:47 +0000

    Another test to trigger travis build

--- test/Graph.test.js
+++ test/Graph.test.js
@@ -2,10 +2,17 @@ var Graph = require('../scripts/Graph.js'),
   expect = require("chai").expect;
 
 describe('Graph', function() {
+
+  var g = new Graph();
+
   describe('Create', function(){
     it("creates a new graph", function(){
-      var g = new Graph();
       expect(g).to.be.a('object');
     });
+
+    it("has nodes", function(){
+      expect(g).to.have.property("nodes").with.length(0);
+    });
   });
+
 });
