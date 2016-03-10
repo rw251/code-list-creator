9f9f02894d6dab8a9f64af62c0f7483b9c7b49e2
Richard Williams <richard.williams2@manchester.ac.uk>
2016-03-10 15:34:29 +0000

    Allow synonyms in line rather than from file

--- index.js
+++ index.js
@@ -32,8 +32,7 @@ if (program.process) {
 
   var meta = program.meta ? file.loadMetadata(program.meta) : {};
 
-  // Read synonyms from file
-  var synonyms = Object.keys(meta).length > 0 ? meta.synonyms : file.getSynonyms();
+  var synonyms = Object.keys(meta).length > 0 ? meta.synonyms : [];
 
   // Read defaults from file
   var defaults = file.getDefaults();
@@ -50,74 +49,92 @@ if (program.process) {
       defaults[key] = result[key];
     });
 
-    meta.version = pkg.version; //TODO maybe a warning if the version has changed
-    meta.name = defaults.name;
-    meta.description = defaults.description;
-    meta.author = defaults.author;
-    meta.terminologies = defaults.terminologies;
-    meta.synonyms = synonyms;
+    var nextStep = function() {
+      meta.version = pkg.version; //TODO maybe a warning if the version has changed
+      meta.name = defaults.name;
+      meta.description = defaults.description;
+      meta.author = defaults.author;
+      meta.terminologies = defaults.terminologies;
+      meta.synonyms = synonyms;
 
-    file.setDefaults(defaults);
+      file.setDefaults(defaults);
 
-    main.resultsAndProcess(meta, function(err, data) {
+      main.resultsAndProcess(meta, function(err, data) {
 
-      console.log("New synonyms file written.");
-      if (err) {
-        console.log(err);
-        return;
-      }
+        console.log("New synonyms file written.");
+        if (err) {
+          console.log(err);
+          return;
+        }
 
-      var g, output = [];
-      var input = function(val) {
-        if (val.children) {
-          g.nodes().forEach(function(node) {
-            if (val.children.indexOf(node) > -1) {
-              g.prop(node, "include", true);
-            } else {
-              g.prop(node, "include", false);
-            }
+        var g, output = [];
+        var input = function(val) {
+          if (val.children) {
+            g.nodes().forEach(function(node) {
+              if (val.children.indexOf(node) > -1) {
+                g.prop(node, "include", true);
+              } else {
+                g.prop(node, "include", false);
+              }
+            });
+          }
+          output.push(g);
+          done();
+        };
+
+        var done = function() {
+          if (data.length === 0) {
+            var outputGraph = Graph.merge(output);
+
+            file.writeCodes(outputGraph);
+
+            meta.synonyms = synonyms;
+            meta.excludedCodes = outputGraph.excluded();
+            meta.includedCodes = outputGraph.included();
+
+            file.writeMetadata(meta);
+
+            process.exit(0);
+          }
+          g = data.pop();
+
+          var checkboxes = g.nodes().map(function(v) {
+            return {
+              "value": v,
+              "name": new Array(g.prop(v, "depth") * 3).join(" ") + g.prop(v, "description").join(" | "),
+              "short": v,
+              "checked": g.prop(v, "include")
+            };
           });
-        }
-        output.push(g);
+
+          inquirer.prompt([{
+            type: "checkbox",
+            name: "children",
+            message: "Review:",
+            choices: checkboxes
+          }], input);
+        };
+
         done();
-      };
+      });
+    };
 
-      var done = function() {
-        if (data.length === 0) {
-          var outputGraph = Graph.merge(output);
-
-          file.writeCodes(outputGraph);
-
-          meta.synonyms = synonyms;
-          meta.excludedCodes = outputGraph.excluded();
-          meta.includedCodes = outputGraph.included();
-
-          file.writeMetadata(meta);
-
-          process.exit(0);
+    var askSynonym = function() {
+      inquirer.prompt({
+        type: "input",
+        name: "synonym",
+        message: "Enter a synonym and hit enter (leave blank to finish)"
+      }, function(answers) {
+        if (answers.synonym === "") {
+          nextStep();
+        } else {
+          synonyms.push(answers.synonym);
+          askSynonym();
         }
-        g = data.pop();
+      });
+    };
 
-        var checkboxes = g.nodes().map(function(v) {
-          return {
-            "value": v,
-            "name": new Array(g.prop(v, "depth") * 3).join(" ") + g.prop(v, "description").join(" | "),
-            "short": v,
-            "checked": g.prop(v, "include")
-          };
-        });
-
-        inquirer.prompt([{
-          type: "checkbox",
-          name: "children",
-          message: "Review:",
-          choices: checkboxes
-        }], input);
-      };
-
-      done();
-
-    });
-
+    if(synonyms.length>0) nextStep();
+    else askSynonym();
   });
 }
